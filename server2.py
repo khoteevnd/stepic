e6eeee1fa8cc58be13c3a130856df4d61478a475
khoteevnd <khoteevnd@gmail.com>
2016-03-31 17:28:27 +0300

    Update server2.py

--- server2.py
+++ server2.py
@@ -12,46 +12,46 @@ _port = 2222
 _maxClient = 11
 _recvBuffer = 1024
 
-def printd (aString):
+def printd(aString):
     if debug:
         print aString
 
-class talkToClient (threading.Thread):
+class talkToClient(threading.Thread):
     def __init__(self, clientSock, addr):
         self.clientSock = clientSock
         self.addr = addr
         threading.Thread.__init__(self)
-    def run (self):
+    def run(self):
         while True:
-            recvData = self.clientSock.recv (_recvBuffer)
+            recvData = self.clientSock.recv(_recvBuffer)
             if not recvData:
-                self.clientSock.send ('bye')
+                self.clientSock.send('bye')
                 break
-            printd('Client ' + str (self.addr) + ' say "' + str (recvData) + '"')
-            self.clientSock.send (recvData)
+            printd('Client ' + str(self.addr) + ' say "' + str(recvData) + '"')
+            self.clientSock.send(recvData)
             if recvData == "close":
                 break
-        self.clientSock.close ()
+        self.clientSock.close()
 
-_connector = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-_connector.bind ((str(_host), int(_port)))
-_connector.listen (int(_maxClient))
+_connector = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+_connector.bind((str(_host), int(_port)))
+_connector.listen(int(_maxClient))
 
 while _running:
-    printd ('Running on ' + _host + ':' + str (_port) + '.')
-    channel, details = _connector.accept ()
-    printd ('Conect on : ' + str (details))
-    #talkToClient (channel, details).start ()
+    printd('Running on ' + _host + ':' + str(_port) + '.')
     clientSock, addr = _connector.accept()
+    printd('Conect on : ' + str (details))
+    #talkToClient (channel, details).start ()
+    #clientSock, addr = _connector.accept()
     while True:
         recvData = clientSock.recv(_recvBudffer)
         if not recvData:
-                clientSock.send ('bye')
+                clientSock.send('bye')
                 break
-        printd('Client ' + str (self.addr) + ' say "' + str (recvData) + '"')
-        clientSock.send (recvData)
+        printd('Client ' + str(addr) + ' say "' + str(recvData) + '"')
+        clientSock.send(recvData)
         if recvData == "close":
                 break
-    self.clientSock.close ()
+    self.clientSock.close()
 
-_connector.close ()
+_connector.close()
