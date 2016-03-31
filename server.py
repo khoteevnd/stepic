d5bd6b16881f65ecd8b7315323400bab3e8bd6ff
khoteevnd <khoteevnd@gmail.com>
2016-03-31 12:58:21 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,10 +12,10 @@ def myrecv(sock, msglen):
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
+while True:
+  conn, addr = s.accept()
   while True:
-    conn, addr = s.accept()
-    while True:
-      data = myrecv(conn, 1024)
-      if not data: break
-      conn.send(data)
-    conn.close()
+    data = myrecv(conn, 1024)
+    if not data: break
+    conn.send(data)
+  conn.close()
