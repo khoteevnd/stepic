e500423eb0cdab8a143e85ad162efeb674d302ae
khoteevnd <khoteevnd@gmail.com>
2016-03-31 15:13:13 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
-import socket, sys
+import socket, sys                                                               
 
-def myrecv(sock, msglen):
-  msg = ''
-  while len(msg) < msglen:
-    chunk = sock.recv(msglen-len(msg))
-    if chunk == '':
-      raise RuntimeError("broken")
-    msg = msg + chunk
-  return msg
+def myrecv(sock, msglen):                                                        
+  msg = ''                                                                       
+  while len(msg) < msglen:                                                       
+    chunk = sock.recv(msglen-len(msg))                                           
+    if chunk == '':                                                              
+      print("chunk = "+chunk)                                                    
+      raise RuntimeError("broken")                                               
+    msg = msg + chunk                                                            
+  return msg                                                                     
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                            
+s.bind(('0.0.0.0', 2222))                                                        
+s.listen(10)                                                                     
+ 
+data = ""                                                                        
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2222))
-s.listen(10)
-while True:
-  conn, addr = s.accept()
-  while True:
-    data = myrecv(conn, 64)
-    if not data: break
-    conn.send(data)
-  conn.close()
+while True:                                                                      
+  conn, addr = s.accept()                                                        
+  print("Conect: "+ str(addr))                                                   
+  while True:                                                                    
+    data += conn.recv(conn, 1024)                                                
+    print(data)                                                                  
+    if data == 'close': break                                                    
+    conn.send(data)                                                              
+  conn.close()    
