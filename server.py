93de53b2a288f9e3cf39ea80f2577d47ff4e4afb
khoteevnd <khoteevnd@gmail.com>
2016-03-31 12:49:26 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+def myrecv(sock, msglen):
+  msg = ''
+  while len(msg) < msglen:
+    chunk = sock.recv(msglen-len(msg))
+    if chunk == '':
+      raise RuntimeError("broken")
+    msg = msg + chunk
+  return msg
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+  while True:
+    conn, addr = s.accept()
+    while True:
+      data = myrecv(conn, 1024) //conn.recv(1024)
+      if not data: break
+      conn.send(data)
+    conn.close()
